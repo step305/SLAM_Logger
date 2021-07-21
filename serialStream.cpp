fe13335bb01cadc084c3953ae9989d504192dfb7
step305 <step305@mail.ru>
2021-07-21 11:23:35 +0300

    0.1

--- serialStream.cpp
+++ serialStream.cpp
@@ -2,3 +2,14 @@
 // Created by step305 on 21.07.2021.
 //
 #include "serialStream.h"
+
+enum ParserStates {
+    WAIT_DLE1,
+    WAIT_DATA,
+    WAIT_DLE2
+};
+
+class UARTParser{
+public:
+    UARTParser()
+};
--- serialStream.h
+++ serialStream.h
@@ -5,4 +5,18 @@
 #ifndef SLAM_LOGGER_SERIALSTREAM_H
 #define SLAM_LOGGER_SERIALSTREAM_H
 
+#pragma pack(1)
+typedef struct{
+    float e1[3];
+    float e2[3];
+    float e3[3];
+    float e4[3];
+    float e5[3];
+    unsigned char check_sum;
+} ReportPacketStructure;
+#pragma pack()
+
+void parce_uart_input (PKT *rpt, unsigned char newbyte);
+unsigned char rpt_0xAA (PKT *rpt, ReportPacketStructure *pack);
+
 #endif //SLAM_LOGGER_SERIALSTREAM_H
